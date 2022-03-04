6eb0a83c8aae75346cfca9f856162cba3852c9c8
Connor Mackay <connor.mackay@gmail.com>
2022-03-04 10:48:01 -0500

    begin building signup mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -9,7 +9,16 @@ const UserType = require('./types/user_type');
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, args, request) {
+         
+      }
+    }
   }
 });
 
